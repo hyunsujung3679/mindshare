0ae7fc9eeeddb12bf5cf6fe806c725ccc00e7c5a
hsjung <hyunsujung3679@gmail.com>
2024-10-23 23:19:10 +0900

    initial commit

---
+++ api/src/main/java/com/hsj/aft/api/controller/PostController.java
@@ -0,0 +1,4 @@
+package com.hsj.aft.api.controller;
+
+public class PostController {
+}
---
+++ api/src/main/java/com/hsj/aft/api/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.hsj.aft.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class UserController {
+
+}

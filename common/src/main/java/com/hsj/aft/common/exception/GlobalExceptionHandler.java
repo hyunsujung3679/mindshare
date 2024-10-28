2b955a660c71251aeb3f8b554a898a633d682428
hsjung <hyunsujung3679@gmail.com>
2024-10-29 00:45:00 +0900

    API 없을 경우, HTTP METHOD가 다를경우, BAD_REQUEST일 경우 ERROR 처리

--- common/src/main/java/com/hsj/aft/common/exception/GlobalExceptionHandler.java
+++ common/src/main/java/com/hsj/aft/common/exception/GlobalExceptionHandler.java
@@ -2,17 +2,25 @@ package com.hsj.aft.common.exception;
 
 import com.hsj.aft.common.dto.CommonResponse;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
+import java.util.Locale;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final MessageSource messageSource;
 
     @ExceptionHandler(DuplicateIdException.class)
     public ResponseEntity<CommonResponse> handleDuplicateIdException(DuplicateIdException e) {
@@ -43,5 +51,25 @@ public class GlobalExceptionHandler {
                 .toList();
 
         return ResponseEntity.ok(CommonResponse.error(String.valueOf(HttpStatus.BAD_REQUEST.value()), errors));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommonResponse> handleException(Exception e, Locale locale) {
+        if (e instanceof HttpRequestMethodNotSupportedException) {
+            return ResponseEntity.ok(CommonResponse.error(
+                    String.valueOf(HttpStatus.METHOD_NOT_ALLOWED.value()),
+                    messageSource.getMessage("message.method.not.allowed", null, locale)
+            ));
+        } else if (e instanceof HttpMessageNotReadableException) {
+            return ResponseEntity.ok(CommonResponse.error(
+                    String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                    messageSource.getMessage("message.bad.request", null, locale)
+            ));
+        } else {
+            return ResponseEntity.ok(CommonResponse.error(
+                    String.valueOf(HttpStatus.NOT_FOUND.value()),
+                    messageSource.getMessage("message.api.not.found", null, locale)
+            ));
+        }
     }
 }
